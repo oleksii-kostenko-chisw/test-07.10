6c6b9f93ae36d855986ae7e6d670154e8a1ec09d
Aleksei <alexysonny@gmail.com>
2020-10-07 22:06:30 +0300

    feat: add sign in

---
+++ src/app/core/interceptors/fake-backend.interceptor.ts
@@ -0,0 +1,55 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { AuthDTO } from '@auth/models/auth.dto';
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const { url, method, body } = request;
+
+    return of(null).pipe(mergeMap(handleRoute));
+    function handleRoute(): Observable<HttpEvent<unknown>> {
+      switch (true) {
+        case url.endsWith('/api/auth/sign-in') && method === 'POST':
+          return handleAuth();
+        default:
+          return next.handle(request);
+      }
+    }
+
+    function ok(body?) {
+      return of(new HttpResponse({ status: 200, body }));
+    }
+
+    function handleAuth(): Observable<HttpEvent<unknown>> {
+      const { login } = body as AuthDTO;
+      const responseBody = {
+        id: 1,
+        username: login.split('@')[0],
+        email: login,
+      };
+
+      return of(new HttpResponse({ status: 200, body: responseBody }));
+    }
+  }
+}
+
+export const fakeBackendProvider: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: FakeBackendInterceptor,
+  multi: true,
+};
---
+++ src/app/features/auth/sign-in/sign-in.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthDTO } from "@auth/models/auth.dto";
+import { SignInService } from './sign-in.service';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss'],
+  providers: [SignInService],
+})
+export class SignInComponent implements OnInit {
+  public signInForm: FormGroup;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _signInService: SignInService
+  ) {}
+
+  ngOnInit(): void {
+    this._formInit();
+  }
+
+  private _formInit(): void {
+    this.signInForm = this._formBuilder.group({
+      login: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  public get isSubmitDisabled(): boolean {
+    return !this.signInForm.valid;
+  }
+
+  public submitSignInForm(): void {
+    this._signInService
+      .signIn(this.signInForm.value as AuthDTO)
+      .subscribe((resp: any) => console.log('::: resp ', resp));
+  }
+}
